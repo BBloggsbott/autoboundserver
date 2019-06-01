36401a96a8d6cc5398d8b4a3e4fe9871ba526887
Vishal R <vishal27.rs@outlook.com>
2019-06-01 11:09:24 +0530

    Add generateNodesTest endpoint

--- autoboundserver/autoboundserver.py
+++ autoboundserver/autoboundserver.py
@@ -1,4 +1,6 @@
-from flask import Flask
+import json
+
+from flask import Flask, request
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -7,3 +9,12 @@ app.config.from_object(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def foo():
     return "Lorem Ipsum"
+
+
+@app.route('/generateNodesTest', methods=['GET', 'POST'])
+def generate_nodes_test():
+    data = request.args.get('data')
+    data = json.loads(data)
+    if(data['verification']=="generateNodesTest"):
+        return "message exchange works"
+    return "message error problem"
