cde5a4f96bf99f2d2b9c0743069bf72617296670
Vishal R <vishal27.rs@outlook.com>
2019-06-01 09:54:39 +0530

    Reformat Code

--- autoboundserver/autoboundserver.py
+++ autoboundserver/autoboundserver.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, session, g, redirect, url_for, abort, \
-render_template, flash, make_response
+from flask import Flask
 
-app = Flask(__name__) 
+app = Flask(__name__)
 app.config.from_object(__name__)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def foo():
